a9441e50888d95e0f66146f91a1f09efefc32687
E1iza95 <93055344+E1iza95@users.noreply.github.com>
2023-05-08 19:59:44 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,57 @@
+//Actividad_1: "Cocinar",
+const Horario1= "6:00 am - 7:00 am";
+//Actividad_2: "Pasear a mi mascota":
+const Horario2= "7:00 am - 8:00 am";
+//Actividad_3: "Trabajar",
+const Horario3= "9:00 am - 5:00 pm";
+//Actividad_4: "Almorzar",
+const Horario4= "1:00 pm - 2:00 pm";
+//Actividad_5: "Estudiar",
+const Horario5= "5:00 pm - 6:00 pm";
+//Actividad_6: "Ver series",
+const Horario6= "8:00 pm - 9:00 pm";
+//Actividad_7: "Escuchar musica",
+const Horario7= "Mientras hago actividades secundarias";
+//Actividad_8: "Hacer ejercicio",
+const Horario8= "5:00 pm - 6:00 pm";
+//Actividad_9: "Leer",
+const Horario9= "8:00 pm - 9:00 pm";
+//Actividad_10: "Hacer compras",
+const Horario10= "11:00 am - 12:00 am";
+
+
+
+const actividades = [Horario1, Horario2, Horario3, Horario4, Horario5, Horario6, Horario7, Horario8, Horario9, Horario10]
+
+
+
+console.log(actividades)
+
+
+
+
+/*const actividades = {
+    Actividad_1: "Cocinar",
+    Horario1: "6:00 am - 7:00 am",
+    Actividad_2: "Pasear a mi mascota",
+    Horario2: "7:00 am - 8:00 am",
+    Actividad_3: "Trabajar",
+    Horario3: "9:00 am - 5:00 pm",
+    Actividad_4: "Almorzar",
+    Horario4: "1:00 pm - 2:00 pm",
+    Actividad_5: "Estudiar",
+    Horario5: "5:00 pm - 6:00 pm",
+    Actividad_6: "Ver series",
+    Horario6: "8:00 pm - 9:00 pm",
+    Actividad_7: "Escuchar musica",
+    Horario7: "Mientras hago actividades secundarias",
+    Actividad_8: "Hacer ejercicio",
+    Horario8: "5:00 pm - 6:00 pm",
+    Actividad_9: "Leer",
+    Horario9: "8:00 pm - 9:00 pm",
+    Actividad_10: "Hacer compras",
+    Horario10: "11:00 am - 12:00 am",
+}
+
+console.log(actividades)
+*/
